04efafc3dc9721f4a240e13def08bc23e5e7ea14
keven_p@rediffmail.com <keven_p@rediffmail.com>
2024-08-28 12:47:58 +0000

    new changes

--- Example/delta_live1.sql
+++ Example/delta_live1.sql
@@ -3,7 +3,7 @@ CREATE OR REFRESH STREAMING TABLE events_bronze
 COMMENT "RAW Data from Kafka in JSON Format"
 AS
 SELECT current_timestamp() processing_time, input_file_name() source_file,*
-FROM   cloud_files("${source}/events_kafka","json",map("cloudFiles.inferColumnTypes", "true"))
+FROM   cloud_files("dbfs:/mnt/dbacademy-datasets/data-engineer-learning-path/v04/ecommerce/raw/events-kafka/","json",map("cloudFiles.inferColumnTypes", "true"))
 
 -- COMMAND ----------
 
@@ -21,3 +21,4 @@ AS
 SELECT date(event_timestamp) as event_date, 
 count(1) as daily_events_total
 FROM LIVE.events_silver
+group by date(event_timestamp)
