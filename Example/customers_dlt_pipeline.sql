0cb74aa9ca28d77ca69b29be9dd1ac427b859590
keven_p@rediffmail.com <keven_p@rediffmail.com>
2024-08-28 15:47:38 +0000

    zip code typo

--- Example/customers_dlt_pipeline.sql
+++ Example/customers_dlt_pipeline.sql
@@ -15,7 +15,7 @@ CREATE STREAMING TABLE customers_bronze_clean
 CONSTRAINT valid_operation EXPECT(operation IS NOT NULL) ON VIOLATION DROP ROW,
 CONSTRAINT valid_name EXPECT(name IS NOT NULL or operation = "DELETE"),
 CONSTRAINT valid_address EXPECT((address IS NOT NULL 
-and city IS NOT NULL and state IS NOT NULL and zip IS NOT NULL) or operation ="DELETE"),
+and city IS NOT NULL and state IS NOT NULL and zip_code IS NOT NULL) or operation ="DELETE"),
 CONSTRAINT valid_email EXPECT (
   rlike(email, '^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$') or 
   operation = "DELETE") ON VIOLATION DROP ROW
